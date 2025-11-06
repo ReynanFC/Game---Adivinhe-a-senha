0f58575e2306311640001b8b3d07aecefe498bde
Reynan <reynandan11@gmail.com>
2025-11-06 20:17:20 -0300

    Melhora na organização e implementação na dica

--- Atividade-Front/main.js
+++ Atividade-Front/main.js
@@ -8,90 +8,107 @@ let tentativas = 0;
 
 esconderTrofeu();
 const numeroGerado = numeroSorteado(); 
-
 limparInputs();
 
 form.addEventListener('submit', (evento) => {
   evento.preventDefault();
-  verificarNumero();
-
-  let acertos = verificarAcerto();
-  verificarGanhou(acertos);
+  processarTentativa();
 });
 
+butaoEnviar.addEventListener('click', () => {
+  if (butaoEnviar.innerText === 'Reiniciar') {
+    location.reload();
+  }
+});
+
+function processarTentativa() {
+  const numero = obterNumeroDigitado();
+  if (!numeroEhValido(numero)) {
+    mostrarMensagem('O número deve ser acima de 100', 'errou');
+    setTimeout(() => limparInputs(), 2000);
+    return;
+  }
+
+  const acertos = verificarAcerto(numero);
+  if (acertos === 3) {
+    encerrarJogo();
+  } else {
+    setTimeout(() => limparInputs(), 1000);
+  }
+
+  tentativas++;
+  mostrarTentativas.innerText = tentativas;
+
+}
+
+function obterNumeroDigitado() {
+  return Number([...inputs].map(i => i.value).join(''));
+}
 
 function numeroSorteado() {
   const numero = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
   return String(numero).split('').map(Number);
 }
 
-function verificarNumero() {
-  const numero = [...inputs].map(i => i.value).join('');
-  if (numero < 100 || numero > 999) {
-    mensagem.classList.add('errou');
-    mensagem.innerText = 'O número deve ser entre 100 e 999.';
-  }
+function numeroEhValido(numero) {
+  return numero >= 100;
 }
 
 function limparInputs() {
-  
-  inputs.forEach(input =>  {
+  inputs.forEach(input => {
     input.value = '';
-    input.classList.remove('acertou');
-    input.classList.remove('errou');
-});
-
+    input.classList.remove('acertou', 'errou');
+  });
+  mensagem.innerText = '';
+  mensagem.classList.remove('acertou', 'errou', 'dica');
   inputs[0].focus();
 }
 
-function verificarAcerto() {
+function dica(numero) {
+  if (numero > Number(numeroGerado.join(''))) {
+    mostrarMensagem('O número sorteado é menor', 'dica');
+  } else if (numero < Number(numeroGerado.join(''))) {
+    mostrarMensagem('O número sorteado é maior', 'dica');
+  }
+}
+
+function verificarAcerto(numero) {
   let qtdAcertos = 0;
 
   inputs.forEach((input, index) => {
-    if (Number(input.value) === numeroGerado[index]) {
-      input.classList.add('acertou');
-      qtdAcertos++;
-    } else {
-      input.classList.add('errou');
-    }
+    const acertou = Number(input.value) === numeroGerado[index];
+    marcaInput(input, acertou);
+    if (acertou) qtdAcertos++;
   });
 
-  if (qtdAcertos > 0) {
-    mensagem.classList.add('acertou');
-    mensagem.innerText = `${qtdAcertos} acerto(s)`;
-  } else {
-    mensagem.innerText = `Você não acertou nenhum número :/`;
-  }
-
-  tentativas++;
-  mostrarTentativas.innerText = tentativas;
-
+  if (qtdAcertos < 3) dica(numero);
   return qtdAcertos;
 }
 
-function verificarGanhou(qtdAcertos) {
-  if (qtdAcertos === 3) {
-    mensagem.classList.add('acertou');
-    mensagem.innerText = `Parabéns, você ganhou!`;
-    inputs.forEach(input => input.disabled = true);
-    mostrarTrofeu();
-    butaoEnviar.innerText = `Reniciar`;
-    reniciar();
-  } else {
-    setTimeout(() => limparInputs(), 1000);
-  }
+function encerrarJogo() {
+  mostrarMensagem('Parabéns, você ganhou!', 'acertou');
+  inputs.forEach(input => input.disabled = true);
+  mostrarTrofeu();
+  butaoEnviar.innerText = 'Reiniciar';
 }
 
-function reniciar() {
-    butaoEnviar.addEventListener('click', () => location.reload());
+function esconderTrofeu() {
+  imagemTrofeu.forEach(imagem => imagem.style.display = 'none');
 }
 
-function esconderTrofeu () {
-    imagemTrofeu.forEach((imagem) => imagem.style.display = 'none');
+function mostrarTrofeu() {
+  imagemTrofeu.forEach(imagem => imagem.style.display = 'block');
 }
 
-function mostrarTrofeu () {
-    imagemTrofeu.forEach((imagem) => imagem.style.display = 'block');
+function mostrarMensagem(texto, tipo) {
+  mensagem.className = '';
+  mensagem.innerText = texto;
+  mensagem.classList.add(tipo);
+}
+
+function marcaInput(input, acertou) {
+  input.classList.remove('acertou', 'errou');
+  input.classList.add(acertou ? 'acertou' : 'errou');
 }
 
 console.log(numeroGerado);
